d041b394106c7582f8ffb28cc48a185e4c633c15
Atharv Tripathi <atharv.tripathi012@gmail.com>
2026-09-10 00:48:29 +0530

    Add STL vector practice

---
+++ Vectrorprob1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v;
+v.push_back(10);
+v.push_back(20);
+v.push_back(30);
+v.push_back(40);
+v.push_back(50);
+    int i=0;
+for(i=0;i<v.size();i++){
+cout<<v[i];
+}
+    return 0;
+}
